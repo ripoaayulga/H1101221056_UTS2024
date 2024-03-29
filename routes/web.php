750fb6317007488c8b91108c128c7481b8b5e374
ripoaayulga <rivoaayulga@gmail.com>
2024-03-29 19:42:51 +0700

    aaa
    aaaa

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/jabatan', function () {
+    return view('jabatan');
+});
+
+Route::get('/presensi', function () {
+    return view('presensi');
+});
